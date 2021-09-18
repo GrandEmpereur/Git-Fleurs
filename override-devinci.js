da9284660b51e645ab49e6d0a76ff19ff415f10d
Patoche <bartosikpatrick2@gmail.com>
2021-09-18 09:39:15 +0200

    :lipstick: Update Ui social-box

--- override-devinci.js
+++ override-devinci.js
@@ -1,1 +1,9 @@
 document.getElementsByTagName("body")[0].classList.add("override")
+
+
+var i = 1
+document.querySelectorAll(".social-box").forEach(function(element){
+    let maClass = 'custom-'+i
+    element.classList.add(maClass)
+    i++
+})
